b83fc3f1adbeadb5edaf866e412969af3fa19db7
Sebb <sebb@apache.org>
2016-04-22 10:24:16 +0000

    Script to get updated copies of JSON files
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/home@986326 90ea9780-b833-de11-8433-001ec94261de

---
+++ tools/update_json.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to get updated copies of Whimsy public JSON files
+
+# Can be run from anywhere
+
+function getNewer() {
+    wget -N -nv https://whimsy.apache.org/public/$1
+}
+
+MYHOME=$(dirname $0)
+cd $MYHOME/../html/public
+
+
+getNewer committee-info.json
+getNewer icla-info.json
+getNewer member-info.json
+getNewer public_ldap_committees.json
+getNewer public_ldap_groups.json
+getNewer public_ldap_people.json
+getNewer public_ldap_services.json
+getNewer public_nonldap_groups.json
